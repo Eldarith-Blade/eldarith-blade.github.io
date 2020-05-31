b4480ce0e1a614e2289b4495e6400776ca9706a2
Peter Ovenden <Peterovenden@outlook.com>
2020-05-31 09:22:01 +0100

    Final_Commit

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
-let countdown = new Date("Jun 1, 2020 00:00:00").getTime(); 
+let slide_index =  0; 
+reveal();
 
-let x = setInterval(function() {
-    let now = new Date().getTime();
-
-    let distance = countdown - now; 
-
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24)).toString();
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString();
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString();
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000).toString();
-
-    document.querySelector(".days-datestamp").innerHTML = `Day: ${days}`;
-    document.querySelector(".time-datestamp").innerHTML = `${hours}:${minutes}:${seconds}`;
-
-    if (distance < 0) {
-        clearInterval(x);
-        document.querySelector(".days-datestamp").innerhtml = ""; 
-        document.querySelector(".time-datestamp").innerHTML = "";
+function reveal() {
+    let integer; 
+    let slides = document.querySelectorAll(".slide");
+    
+    for (integer = 0; integer < slides.length; integer++) {
+        slides[integer].style.display = "none";
     }
-}, 1000);
+    slide_index++;
+
+    if(slide_index > slides.length) {
+        slide_index = 1; 
+    }
+
+    slides[slide_index-1].style.display = "block";
+    setTimeout(reveal, 7000);
+}
+
+document.querySelector('.fas').onclick = function () {
+     let audio = document.getElementById("audio");
+    audio.play();
+}
