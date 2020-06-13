ab12acec7ae9c98db4021dcde4d785cb0888142c
Peter Ovenden <Peterovenden@outlook.com>
2020-06-13 08:47:39 +0100

    Final

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,14 @@
 import * as fetched from './classes/fetch.class.js';
 
+let collapsed = false;
+let splitCollapse = false;  
+
 const update = document.querySelector('#update');
 const filtered = document.querySelector('#searchbar');
+const collapse = document.querySelector('.fa-bars');
+const menu = document.querySelector('.menu-items-container');
+const downloads = document.querySelector('.downloads');
+const split = document.querySelector('.split-menu');
 
 update.addEventListener('click', () => {
     fetched.fetchRowInsertion(); 
@@ -10,5 +17,27 @@ update.addEventListener('click', () => {
 filtered.addEventListener('keyup', () => {
     fetched.filterSearch();
 })
+
+collapse.addEventListener('click', () => {
+    if(collapsed == false) {
+        menu.style.display = 'flex';
+        collapsed = true; 
+    } else {
+        menu.style.display = 'none';
+        collapsed = false; 
+    }
+})
+
+downloads.addEventListener('click', () => {
+    if(splitCollapse == false) {
+        split.style.display = 'flex';
+        splitcollapsed = true; 
+    } else {
+        split.style.display = 'none';
+        splitcollapsed = false; 
+    }
+})
+
+
 
 
